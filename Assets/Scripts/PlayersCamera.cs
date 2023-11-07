b435dcb115fac9a9e42e00a8f571f908b0d6015a
Nerea <nerea.martinez@enti.cat>
2023-11-07 13:21:07 +0100

    Cámara hecha suuu

--- Assets/Scripts/PlayersCamera.cs
+++ Assets/Scripts/PlayersCamera.cs
@@ -8,7 +8,7 @@ public class PlayersCamera : MonoBehaviour
 {
     public List<Transform> targets;
 
-    public Vector3 offset = new Vector3(0, 3, -15);
+    public Vector3 offset = new Vector3(0, 3, -30);
 
     private void Update()
     {
