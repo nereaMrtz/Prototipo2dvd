a6dfb8fd69bb2728523041891ad77aa487a8f815
Pau Amigó <pau.amigo@enti.cat>
2023-12-12 12:44:12 +0100

    Coses varies

---
+++ Assets/Scripts/Players/PlayerContoller.cs
@@ -0,0 +1,218 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
+using static UnityEngine.InputSystem.DefaultInputActions;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerContoller : MonoBehaviour
+{
+    [Header("Player Movement")]
+    [SerializeField] private float playerSpeed = 2.0f;
+    private CharacterController controller;
+    private Vector2 movementInput = Vector2.zero;
+    private Vector3 playerVelocity;
+
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    private bool groundedPlayer;
+    private bool jumpInput = false;
+    private bool canWallJump = false;
+
+    [SerializeField] private float maxCoyoteTime = 0.25f;
+    private float coyoteTimer;
+    private bool hasJumped = false;
+    private bool wasGrounded;
+    private bool inCoyote = false;
+
+    [SerializeField] private float inputBufferTime = 0.25f;
+    private float buffer;
+    private bool inputBuffer;
+
+    [Header("Maldición")]
+
+    [SerializeField] PlayerContoller otherPlayer;
+    public bool maldicion ;
+
+
+    // [SerializeField] Material ghost;
+    [SerializeField] Material noMalditoMaterial;
+    [SerializeField] Material malditoMaterial;
+
+    private float distance;
+
+    private AudioManager sound;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        sound = GameObject.FindGameObjectWithTag("AM").GetComponent<AudioManager>();
+        controller.attachedRigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        float aux = context.ReadValue<float>();
+        if (aux != 0f)
+        {
+            jumpInput = true;
+        }
+        else { jumpInput = false; }
+        // hasJumped = context.action.triggered;
+    }
+
+    public void OnMaldicion(InputAction.CallbackContext context)
+    {
+        if (context.action.triggered)
+        {
+            if (!otherPlayer.maldicion)
+            {
+                otherPlayer.ChangeMaldicion();
+                ChangeMaldicion();
+            }
+            sound.maldicion.Play();
+        }
+    }
+
+    // LAYERS
+    // 8: NoMaldito
+    // 9: Maldito
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+
+        if (!groundedPlayer && wasGrounded)
+        {
+            inCoyote = true;
+            coyoteTimer = maxCoyoteTime;
+        }
+
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+            hasJumped = false;
+        }
+
+
+        distance = otherPlayer.transform.position.x - gameObject.transform.position.x;
+
+        //CALCULO DISTANCIAS PLAYERS
+        if (distance > 25)
+        {
+
+        }
+
+        Vector3 move = new Vector3(movementInput.x, -0.5f, .0f);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (jumpInput)
+        {
+            if (groundedPlayer)
+            {
+                Jump();
+            }
+            else if (inCoyote && !hasJumped)
+            {
+                Jump();
+                inCoyote = false;
+            }
+            else
+            {
+                inputBuffer = true;
+                buffer = inputBufferTime;
+            }
+        }
+
+        if (inputBuffer)
+        {
+            if (groundedPlayer)
+                Jump();
+            buffer -= Time.deltaTime;
+            if(buffer < 0)
+            {
+                inputBuffer = false;
+            }
+            
+        }
+
+        //WallJump
+        if (hasJumped && canWallJump)
+        {
+
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        Physics.IgnoreLayerCollision(9, 7, false); // Layer 9: Maldito
+        Physics.IgnoreLayerCollision(8, 7, true); // Layer 8: NoMaldito
+
+        if (maldicion)
+        {
+            gameObject.GetComponentInChildren<SkinnedMeshRenderer>().material = malditoMaterial;
+        }
+
+        if (!maldicion)
+        {
+            gameObject.GetComponentInChildren<SkinnedMeshRenderer>().material = noMalditoMaterial;
+        }
+
+        if (inCoyote)
+        {
+            coyoteTimer -= Time.deltaTime;
+
+            if (coyoteTimer < 0)
+                inCoyote = false;
+        }
+
+        wasGrounded = groundedPlayer;
+
+    }
+
+    void Jump()
+    {
+        playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        sound.jump.Play();
+        hasJumped = true;
+        inputBuffer = false;
+    }
+
+    // this script pushes all rigidbodies that the character touches
+    float pushPower = 2.0f;
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic)
+        {
+            return;
+        }
+
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3)
+        {
+            return;
+        }
+
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, -0.5f);
+
+        body.velocity = pushDir * pushPower;
+
+    }
+
+    void ChangeMaldicion()
+    {
+        maldicion = !maldicion;
+        if (maldicion)
+            this.gameObject.layer = 9;
+        else
+            this.gameObject.layer = 8;
+    }
+}
---
+++ Assets/Scripts/Players/PlayersCamera.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayersCamera : MonoBehaviour
+{
+    public List<Transform> targets;
+
+    public Vector3 offset = new Vector3(0, 3, -30);
+
+    private void Update()
+    {
+        // Debug.Log(targets.Count);
+    }
+
+    private void LateUpdate()
+    {
+        if (targets.Count == 0)
+        {
+            return;
+        }
+
+        Vector3 centerPoint = GetCenterPoint();
+        Vector3 newPosition = centerPoint + offset;
+        transform.position = newPosition;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+            // Debug.Log(targets[i].position);
+        }
+        return bounds.center;
+    }
+
+    public void AddPlayer(PlayerInput player)
+    {
+        Transform aux = player.gameObject.transform;
+
+        targets.Add(aux);
+    }
+}
