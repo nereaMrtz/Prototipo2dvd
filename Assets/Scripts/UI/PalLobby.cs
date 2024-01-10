8aa3e839d7994f4fcbb8396adf4497164713ef09
Pau Amigó <pau.amigo@enti.cat>
2024-01-11 00:18:40 +0100

    aaaaaaaaaaaaaaaaaaaalpha-pre

---
+++ Assets/Scripts/UI/PalLobby.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PalLobby : MonoBehaviour
+{
+    public void PalLoby()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
