f43b93745b84f34e472c177b614c57caa2ce0377
Jordi Climent <jordi.climent@enti.cat>
2024-01-10 17:48:57 +0100

    Just a fade left

--- Assets/Scripts/Managers/L1_M1_Manager.cs
+++ Assets/Scripts/Managers/L1_M1_Manager.cs
@@ -6,17 +6,24 @@ public class L1_M1_Manager : ScenesManager
 {
     [SerializeField] private GameObject ghost1;
     [SerializeField] private GameObject ghost2;
+    [SerializeField] private float timeToChangeScene = 2;
 
     private void Update()
     {
         if(ghost1.layer == 9 && ghost2.layer == 9)
         {
-
+            ChangeScene();
         }
     }
 
     protected override void ChangeScene()
     {        
         base.ChangeScene();
+    }
+
+    IEnumerator WaitThenChangeScene() 
+    {
+        yield return new WaitForSeconds(timeToChangeScene);
+        ChangeScene();
     }
 }
