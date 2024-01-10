50e9f384e64cfa6882ab61a2228c0f81489eff2d
Jordi Climent <jordi.climent@enti.cat>
2024-01-10 20:37:31 +0100

    Musica de fons

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -10,4 +10,20 @@ public class AudioManager : MonoBehaviour
     [SerializeField] public AudioSource wind;
     [SerializeField] public AudioSource door_platform;
     [SerializeField] public AudioSource jump;
+
+    public static AudioManager Instance { get; private set; }
+
+    [HideInInspector] public bool load = false;
+    [SerializeField] private Animator transition;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one AudioManager!" + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
 }
