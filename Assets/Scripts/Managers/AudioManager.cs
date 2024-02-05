f57874da376b69c305d6dc9c3817a2dd5a3e0568
Pau <pau.amigo@enti.cat>
2024-02-05 12:05:40 +0100

    Audio Mutes

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -14,6 +14,10 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioMixer mixer; 
     public static AudioManager Instance { get; private set; }
 
+    bool masterMute=false;
+    bool bgMute=false;
+    bool sfxMute=false;
+    
     [HideInInspector] public bool load = false;
     [SerializeField] private Animator transition;
     private void Awake()
@@ -46,5 +50,45 @@ public class AudioManager : MonoBehaviour
 
     }
 
-    //mixer.SetFloat("Master", -80f);
+    public void MuteMaster()
+    {
+        if (!masterMute)
+        {
+            mixer.SetFloat("Master", -80f);
+            masterMute = true;
+        }
+        else
+        {
+            mixer.SetFloat("Master", 0f);
+            masterMute = false;
+        }
+    }
+
+    public void MuteBackground()
+    {
+        if (!bgMute)
+        {
+            mixer.SetFloat("BackgroundMusic", -80f);
+            bgMute = true;
+        }
+        else
+        {
+            mixer.SetFloat("BackgroundMusic", 0f);
+            bgMute = false;
+        }
+    }
+
+    public void MuteSFX()
+    {
+        if (!sfxMute)
+        {
+            mixer.SetFloat("SoundEffects", -80f);
+            sfxMute = true;
+        }
+        else
+        {
+            mixer.SetFloat("SoundEffects", 0f);
+            sfxMute = false;
+        }
+    }
 }
