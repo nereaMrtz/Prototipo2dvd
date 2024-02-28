a8ed5e2ec0bef422938d14b93da4e85f30d62010
Pau <pau.amigo@enti.cat>
2024-02-28 11:48:20 +0100

    Coin works
    RN the coins amount is saved in the coin manager

--- Assets/Scripts/Level Obstacles/Coins.cs
+++ Assets/Scripts/Level Obstacles/Coins.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CoinScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
+        Debug.Log("puta");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(other.gameObject.CompareTag("Player1") || other.gameObject.CompareTag("Player2"))
+        {
+            Debug.Log("puta2");
+            CoinManager.Instance.AddCoin();
+            Destroy(this.gameObject);
+        }
     }
 }
