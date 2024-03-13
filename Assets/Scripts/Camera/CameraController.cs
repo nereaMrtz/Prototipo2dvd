f8c152969183fadf2a3e2fcdf47d519eef54eb68
Jordi Climent <45494980+JordiCC1@users.noreply.github.com>
2024-03-13 12:59:56 +0100

    Camera on toca

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -35,8 +35,11 @@ public class CameraController : MonoBehaviour
     {     
         //Debug.Log(target1.transform.position.y + " " + target2.transform.position.y);
         if(Vector3.Distance(target1.transform.position, target2.transform.position) >= 18f)
-        {            
-            SetSplitScreen();            
+        {         
+            if(target1.transform.position.x < target2.transform.position.x)
+                SetSplitScreen1();  
+            else
+                SetSplitScreen2();
         }
         else
         {
@@ -53,7 +56,7 @@ public class CameraController : MonoBehaviour
         */
     }
 
-    void SetSplitScreen()
+    void SetSplitScreen1()
     {
         if(!split)
         {
@@ -83,7 +86,36 @@ public class CameraController : MonoBehaviour
         //    Fade.Instance.FadeIn();
         //}
     }
-
+    void SetSplitScreen2()
+    {
+        if (!split)
+        {
+            Fade.Instance.FadeOut();
+            split = true;
+        }
+        mainCamera.enabled = false;
+        //mainCamera.gameObject.SetActive(false);
+        int cameraAmmount = cameras.Count;
+        float x = -0.5f;
+        for (int i = cameraAmmount-1; i >= 0; i--)
+        {
+            cameras[i].enabled = true;
+            cameras[i].gameObject.SetActive(true);
+            //Set it split
+            float auxX = x;
+            float y = 0.0f;
+            float width = 1;
+            float height = 1;
+            cameras[i].rect = new Rect(auxX, y, width, height);
+            x *= -1;
+        }
+        arrow1.SetActive(true);
+        arrow2.SetActive(true);
+        //if (!split)
+        //{
+        //    Fade.Instance.FadeIn();
+        //}
+    }
     void SetSingleCamera()
     {
         if(split) 
