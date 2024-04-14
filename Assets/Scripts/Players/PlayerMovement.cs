2ac8897ce66d3d2e76e0529545cc792a68390ee7
PauAArjona <pau.arjona@decagames.com>
2024-04-14 21:36:55 +0200

    Player Movement done

--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -7,8 +7,11 @@ using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class PlayerMovement : MonoBehaviour
 {
+    
     [SerializeField] Rigidbody rb;
 
     private Vector2 movementInput;
@@ -24,15 +27,16 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 1f;
     public float groundedRayLength = 0.1f;
     public float gravity = -10.0f;
-    public GameObject groundCheck;
-    public GameObject rightCheck;
-    public GameObject leftCheck;
+    [SerializeField] private GameObject groundCheck;
+    [SerializeField] private GameObject rightCheck;
+    [SerializeField] private GameObject leftCheck;
 
     [Header("Coyote Time")]
     [Tooltip("This marks the time the player will have to jump after it leaves contact with the ground")]
     public float coyoteTime = 0.5f;
     private float coyoteTimer;
     private bool inCoyote = false;
+    private bool hasJumped = false;
     private bool prevGrounded;
 
     [Header("InputBuffer")]
@@ -40,6 +44,9 @@ public class PlayerMovement : MonoBehaviour
     public float inputBuffer = 0.5f;
     private float inputBufferTimer;
     private bool inBuffer = false;
+
+    [Header("Modificators")]
+    public bool movementEnabled = true;
 
     void Start()
     {
@@ -50,8 +57,6 @@ public class PlayerMovement : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
-
-
     }
     public void OnJump(InputAction.CallbackContext context)
     {
@@ -70,89 +75,103 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        #region Movement
-        if (Mathf.Abs(movementInput.x) > 0.01f)
+        if (movementEnabled)
         {
-            targetMovement.x = Mathf.Lerp(targetMovement.x, movementInput.x * topSpeed, Time.fixedDeltaTime * acceleration); ;
-        }
-        else
-            targetMovement.x = Mathf.Lerp(targetMovement.x, 0.0f, Time.fixedDeltaTime * deceleration);
 
-        #endregion
+            #region Gravity
 
-        #region Coyote Time
+            targetMovement.y = rb.velocity.y;
 
-        if (prevGrounded && !IsGrounded())
-        {
-            inCoyote = true;
-            coyoteTimer = coyoteTime;
-        }
-
-        if (inCoyote)
-        {
-            coyoteTimer -= Time.fixedDeltaTime;
-            if (coyoteTimer <= 0.0f)
+            if (!IsGrounded())
             {
+                float gravinc = gravity * Time.fixedDeltaTime;
+                targetMovement.y += gravinc;
+            }
+            else
+            {
+                targetMovement.y = 0.0f;
+                hasJumped = false;
+            }
+
+            #endregion
+
+            #region Movement
+
+            if (Mathf.Abs(movementInput.x) > 0.01f)
+            {
+                targetMovement.x = Mathf.Lerp(targetMovement.x, movementInput.x * topSpeed, Time.fixedDeltaTime * acceleration); ;
+            }
+            else
+                targetMovement.x = Mathf.Lerp(targetMovement.x, 0.0f, Time.fixedDeltaTime * deceleration);
+
+            #endregion
+
+            #region Coyote Time
+
+            if (prevGrounded && !IsGrounded() && !hasJumped)
+            {
+                inCoyote = true;
+                coyoteTimer = coyoteTime;
+            }
+
+            if (inCoyote)
+            {
+                coyoteTimer -= Time.fixedDeltaTime;
+                if (coyoteTimer <= 0.0f)
+                {
+                    inCoyote = false;
+                }
+            }
+
+            #endregion
+
+            #region Input Buffer
+
+            if (jumpInput && !IsGrounded())
+            {
+                inBuffer = true;
+                inputBufferTimer = inputBuffer;
+            }
+
+            if (inBuffer)
+            {
+                inputBufferTimer -= Time.fixedDeltaTime;
+                if (inputBufferTimer <= 0.0f)
+                {
+                    inBuffer = false;
+                }
+            }
+
+            #endregion
+
+            #region Jump
+
+            if ((IsCollidingLeft() && movementInput.x < 0.0f) || (IsCollidingRight() && movementInput.x > 0.0f))
+            {
+                targetMovement.x = 0.0f;
+                Debug.Log("a");
+            }
+
+
+            if ((jumpInput || inBuffer) && (IsGrounded() || inCoyote))
+            {
+
+                rb.AddForce(Vector3.up * jumpForce);
                 inCoyote = false;
-            }
-        }
-
-        #endregion
-
-        #region Input Buffer
-
-        if (jumpInput && !IsGrounded())
-        {
-            inBuffer = true;
-            inputBufferTimer = inputBuffer;
-        }
-
-        if (inBuffer)
-        {
-            inputBufferTimer -= Time.fixedDeltaTime;
-            if (inputBufferTimer <= 0.0f)
-            {
                 inBuffer = false;
+                jumpInput = false;
+                hasJumped = true;
             }
+
+
+
+            #endregion
+
+            rb.velocity = targetMovement;
+
+            prevGrounded = IsGrounded();
+
         }
-
-        #endregion
-
-        #region Jump
-
-        if ((IsCollidingLeft() && movementInput.x<0.0f) || (IsCollidingRight()&& movementInput.x > 0.0f))
-        {
-            targetMovement.x = 0.0f;
-            Debug.Log("a");
-        }
-
-
-        if ((jumpInput || inBuffer) && (IsGrounded() || inCoyote))
-        {
-
-            rb.AddForce(Vector3.up * jumpForce);
-            inCoyote = false;
-            inBuffer = false;
-            jumpInput = false;
-        }
-
-
-        targetMovement.y = rb.velocity.y;
-
-        if (!IsGrounded())
-        {
-            float gravinc = gravity * Time.fixedDeltaTime;
-            targetMovement.y += gravinc;
-        }
-        else
-            targetMovement.y = 0.0f;
-
-        #endregion
-
-        rb.velocity = targetMovement;
-
-        prevGrounded = IsGrounded();
-
 
     }
 
