7964f9b1ac90a0a73affb5f4086159d18e35813e
Pau Amigó <pau.amigo@enti.cat>
2024-04-24 11:03:49 +0200

    Main Menu code fix

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -7,19 +7,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Menus")]
     [SerializeField] GameObject mainMenu;
     [SerializeField] GameObject settingsMenu;
+    [SerializeField] GameObject creditsMenu;
+    [Header("Buttons")]
     [SerializeField] GameObject firstButtonMain;
     [SerializeField] GameObject firstButtonSettings;
-
-    [Header ("Settings Screen")]
-    [SerializeField] GameObject ControlsScreen;
-    [SerializeField] GameObject SoundScreen;
-    [SerializeField] GameObject CreditsScreen;
-    [SerializeField] GameObject ScreenScreen;
+    [SerializeField] GameObject firstButtonCredits;
 
     GameObject currentMenu;
-    GameObject currentSettingScreen;
 
     private void Start()
     {
@@ -39,8 +36,14 @@ public class MainMenu : MonoBehaviour
         currentMenu.SetActive(false);
         settingsMenu.SetActive(true);
         currentMenu = settingsMenu;
-        currentSettingScreen = SoundScreen;
         EventSystem.current.SetSelectedGameObject(firstButtonSettings);
+    }
+    public void OpenCredits()
+    {
+        currentMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+        currentMenu = creditsMenu;
+        EventSystem.current.SetSelectedGameObject(firstButtonCredits);
     }
 
     public void BackToMenu()
@@ -51,33 +54,6 @@ public class MainMenu : MonoBehaviour
         currentMenu = mainMenu;
     }
 
-    public void OpenControls()
-    {
-        currentSettingScreen.SetActive(false);
-        ControlsScreen.SetActive(true);
-        currentSettingScreen = ControlsScreen;
-    }
-
-    public void OpenSound()
-    {
-        currentSettingScreen.SetActive(false);
-        SoundScreen.SetActive(true);
-        currentSettingScreen = SoundScreen;
-    }
-
-    public void OpenCredits()
-    {
-        currentSettingScreen.SetActive(false);
-        CreditsScreen.SetActive(true);
-        currentSettingScreen=CreditsScreen;
-    }
-
-    public void OpenScreen()
-    {
-        currentSettingScreen.SetActive(false);
-        ScreenScreen.SetActive(true);
-        currentSettingScreen=ScreenScreen;
-    }
 
     public void Play()
     {
