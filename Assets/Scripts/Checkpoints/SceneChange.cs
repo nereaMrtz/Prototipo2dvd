8ee0143eae8b2ff0aefddf826d08cfd1530d3f88
Pau Amigó <pau.amigo@enti.cat>
2024-05-06 17:12:12 +0200

    Scene change to lvl3 fixed

--- Assets/Scripts/Checkpoints/SceneChange.cs
+++ Assets/Scripts/Checkpoints/SceneChange.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-    public string nextScene;
-
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(nextScene);
+        ChangeScene();
+    }
+
+    public void ChangeScene()
+    {
+        if (SceneManager.GetActiveScene().name == "L1_M1")
+            SceneManager.LoadScene("L2_M1");
+        else if (SceneManager.GetActiveScene().name == "L2_M1")
+            SceneManager.LoadScene("L3_M1");
     }
 }
