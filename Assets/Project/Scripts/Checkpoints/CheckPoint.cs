b971b45a89f78c64d309de4e3121930132bd2b2a
Jordi Climent <45494980+JordiCC1@users.noreply.github.com>
2024-05-24 13:33:11 +0200

    Test Checkpoints

--- Assets/Project/Scripts/Checkpoints/CheckPoint.cs
+++ Assets/Project/Scripts/Checkpoints/CheckPoint.cs
@@ -11,8 +11,6 @@ public class CheckPoint : MonoBehaviour
     private bool isActive1 = false; 
     private bool isActive2 = false; 
     private bool isOnCamera = false; 
-    private bool player1Passed = false; 
-    private bool player2Passed = false;
     private bool graveMoved = false;
 
     [SerializeField]private Transform grave;
@@ -29,34 +27,32 @@ public class CheckPoint : MonoBehaviour
     {
 
         Debug.Log(this.gameObject + " " + this.GetComponent<IsInCamera>().IsInCameraNow());
-        if (!player1Passed && !isActive1)
-        {
-            player1Passed = true;
-            isActive1 = true;
-            if(!graveMoved)
-            {
-                Vector3 pos = grave.position;
-                pos.y += 2f;
-                grave.DOMoveY(pos.y, 1f);
-                graveMoved = true;
-            }
-            if (AudioManager.Instance.LoadSFX(clipName, clip))
-                AudioManager.Instance.PlaySFX(clipName);
-        }
-        if (!player2Passed && !isActive2) 
-        {
-            player2Passed = true;
-            isActive2 = true;
-            if (!graveMoved)
-            {
-                Vector3 pos = grave.position;
-                pos.y += 2f;
-                grave.DOMoveY(pos.y, 0.7f);
-                graveMoved = true;
-            }
-            if (AudioManager.Instance.LoadSFX(clipName, clip))
-                AudioManager.Instance.PlaySFX(clipName);
-        } 
+        //if (player1Passed && !isActive1)
+        //{
+        //    isActive1 = true;
+        //    if(!graveMoved)
+        //    {
+        //        Vector3 pos = grave.position;
+        //        pos.y += 2f;
+        //        grave.DOMoveY(pos.y, 1f);
+        //        graveMoved = true;
+        //    }
+        //    if (AudioManager.Instance.LoadSFX(clipName, clip))
+        //        AudioManager.Instance.PlaySFX(clipName);
+        //}
+        //if (player2Passed && !isActive2) 
+        //{
+        //    isActive2 = true;
+        //    if (!graveMoved)
+        //    {
+        //        Vector3 pos = grave.position;
+        //        pos.y += 2f;
+        //        grave.DOMoveY(pos.y, 0.7f);
+        //        graveMoved = true;
+        //    }
+        //    if (AudioManager.Instance.LoadSFX(clipName, clip))
+        //        AudioManager.Instance.PlaySFX(clipName);
+        //} 
         if(this.GetComponent<IsInCamera>().IsInCameraNow() && isActive1) 
         {
             //Debug.Log("Active for 1");
@@ -79,14 +75,17 @@ public class CheckPoint : MonoBehaviour
     {
         if (coll.CompareTag("Player1"))
         {
-            player1Passed = true;
+            isActive1 = true;
             CM.SetLastCheckPointPos(coll.GetComponent<Transform>().position);
         }
         else if(coll.CompareTag("Player2"))
         {
+            isActive2 = true;
             CM.SetLastCheckPointPos2(coll.GetComponent<Transform>().position);
-            player2Passed = true;
             //CM.SetLastCheckPointPosGhost2(coll.GetComponent<Transform>().position);
         }
     }
+
+
+   
 }
